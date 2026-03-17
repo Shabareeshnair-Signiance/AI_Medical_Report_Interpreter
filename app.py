fcded80c5574c5954fcc73410f63c5cd891417b6
Shabareesh-Signiance <shabareesh.nair@signiance.com>
2026-03-17 10:49:17 +0530

    App file updated with code and fixed bugs

--- app.py
+++ app.py
@@ -16,7 +16,11 @@ from storage.database import (
 from logger_config import logger
 
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="layouts",
+    static_folder=""
+)
 
 # Folder to store uploaded PDFs
 UPLOAD_FOLDER = "data/uploads"
@@ -24,35 +28,37 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-# initialize database at startup
+# Initialize database
 init_database()
 
-# Loading LangGraph once
+# Load LangGraph
 graph = build_medical_graph()
 
-@app.route("/", methods = ["GET", "POST"])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
 
     try:
         if request.method == "POST":
+
             file = request.files.get("file")
 
             if not file:
                 return "No file uploaded"
-            
-            # Saving file
+
+            # Save file
             file_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
             file.save(file_path)
 
             logger.info(f"File uploaded: {file.filename}")
 
-            # generating file hash
+            # Generate hash
             file_hash = generate_file_hash(file_path)
 
             if not file_hash:
                 return "Error generating file hash"
-            
-            # checking database (CACHE)
+
+            # Check cache
             cached_result = check_existing_report(file_hash)
 
             if cached_result:
@@ -60,47 +66,42 @@ def index():
 
                 return render_template(
                     "main.html",
-                    medical_data = cached_result["medical_data"],
-                    analysis = cached_result["analysis"],
-                    explanation = cached_result["explanation"],
-                    guidance = cached_result["guidance"]
+                    medical_data=cached_result["medical_data"],
+                    analysis=cached_result["analysis"],
+                    explanation=cached_result["explanation"],
+                    guidance=cached_result["guidance"]
                 )
-            
-            # Running the Full Pipeline
-            logger.info("Running full Pipeline")
 
-            # Reading PDF
+            # Run pipeline
+            logger.info("Running full pipeline")
+
             report_text = read_pdf(file_path)
-
-            # Parse medical values
             medical_data = parse_medical_report(report_text)
 
-            # Preparing the state
             state = {
                 "medical_data": medical_data
             }
 
-            # running langgraph
             result = graph.invoke(state)
 
-            # saving result in DB
             save_report(file_hash, result)
 
             logger.info("Pipeline completed successfully")
 
             return render_template(
                 "main.html",
-                medical_data = result["medical_data"],
-                analysis = result["analysis"],
-                explanation = result["explanation"],
-                guidance = result["guidance"]
+                medical_data=result["medical_data"],
+                analysis=result["analysis"],
+                explanation=result["explanation"],
+                guidance=result["guidance"]
             )
 
         return render_template("main.html")
-        
+
     except Exception as e:
         logger.error(f"App error: {str(e)}")
         return "Something went wrong"
-    
+
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
